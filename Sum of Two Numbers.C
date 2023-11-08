1f81f9631a95052e9da167a12afebc25990a28d2
Nahidul Islam Rony <139171376+niRony02@users.noreply.github.com>
2023-11-08 19:42:02 +0600

    Update Sum of Two Numbers.C

--- Sum of Two Numbers.C
+++ Sum of Two Numbers.C
@@ -1,5 +1,7 @@
-#include<stdio.h>
+/*Task 01: Sum of Two Numbers
+Write a function add that takes two integers as parameters and returns their sum.*/
 
+#include<stdio.h>
 int add(int num1, int num2){
     int sum = num1 + num2;
     return sum;
