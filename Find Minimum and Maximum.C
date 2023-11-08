28c113d8fb4e8d6f3e9cca9a792bde358d57d1dc
Nahidul Islam Rony <139171376+niRony02@users.noreply.github.com>
2023-11-08 19:45:37 +0600

    Update Find Minimum and Maximum.C

--- Find Minimum and Maximum.C
+++ Find Minimum and Maximum.C
@@ -1,3 +1,8 @@
+/*Task 05: Find Minimum and Maximum
+Create two functions: findMin and findMax.
+Each should take an array of integers and the array's size and return the minimum and maximum value, respectively.
+ */
+
 #include <stdio.h>
 int findMin(int arr[], int size)
 {
@@ -8,8 +13,7 @@ int findMin(int arr[], int size)
         }
         }
     return min;
-}
-
+    }
 int findMax(int arr[], int size) {
     int max = arr[0];
     for (int i = 1; i < size; i++) {
@@ -19,24 +23,17 @@ int findMax(int arr[], int size) {
 }
     return max;
 }
-
- int main() {
+int main() {
     int size;
         scanf("%d", &size);
-
     int arr[size];
-
         printf("Enter the numbers:\n");
     for (int i = 0; i < size; i++) {
         scanf("%d", &arr[i]);
     }
-
-
     printf("Minimum value of array: %d\n", findMin(arr, size));
 
     printf("Maximum value of array: %d\n", findMax(arr, size));
-
-    return 0;
-
+return 0;
 }
 
