064c7fc8f6163180d293875519e978bc8363d425
Nahidul Islam Rony <139171376+niRony02@users.noreply.github.com>
2023-11-08 19:42:39 +0600

    Update Calculate Average.C

--- Calculate Average.C
+++ Calculate Average.C
@@ -1,14 +1,20 @@
-#include<stdio.h>
+/*Task 02:  Calculate Average
+Define a function calculateAverage that takes two floats as arguments
+and returns the average of these two numbers.
+ */
 
-float avg(float a, float b) {
+#include<stdio.h>
+float avg(float a, float b){
+
     return (a + b) / 2;
 }
 
-int main() {
+int main()
+{
     float num1, num2;
-    scanf("%f %f", &num1, &num2);
+        scanf("%f %f", &num1, &num2);
     float average = avg(num1, num2);
-    printf("Average = %0.3f", average);
+        printf("Average = %0.3f", average);
 
     return 0;
 }
